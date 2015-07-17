efd1c8ecb9effd22abd484d3a2d52ec12b73ab3f
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2015-07-18 04:49:40 +0530

    Connect to the sim service

--- tubing/static/js/controllers/SimulationCtrl.js
+++ tubing/static/js/controllers/SimulationCtrl.js
@@ -1,11 +1,17 @@
-angular.module('fishApp').controller('SimulationCtrl', ['$scope', '$log', '$routeParams', '$location', 'Maps',
-    function($scope, $log, $routeParams, $location, Maps) {
+angular.module('fishApp').controller('SimulationCtrl', ['$scope', '$log', '$routeParams', '$location', 'Maps', 'Sim',
+    function($scope, $log, $routeParams, $location, Maps, Sim) {
         $scope._load_map = function() {
             Maps.fetch_map_data($scope.map_name, $scope._map_load_success, $scope._map_load_fail);
         }
     
         $scope._map_load_success = function(data, status, headers, config) {
             $scope.map_data = data;
+
+            Sim.wait_for_connection(function() {
+                // Now that we have all the data, we can start the simulation
+                debugger
+                Sim.start_simulation($scope.map_name);
+            });
         }
 
         $scope._map_load_fail = function(data, status, headers, config) {
--- tubing/static/js/services/SimService.js
+++ tubing/static/js/services/SimService.js
@@ -1,7 +1,38 @@
-angular.module('fishApp').factory('Sim', function() {
+angular.module('fishApp').factory('Sim', ['$location', '$log', function($location, $log) {
     var sim = {}
-    sim.method1 = function() {
-        $log.info("Successful call in service");
+
+    sim.ws = new WebSocket('ws://'+$location.host()+':'+$location.port()+'/sim');
+    sim._ws_open = false;
+
+
+    sim.ws.onopen = function() {
+        sim._ws_open = true;
     }
+
+    sim.wait_for_connection = function(callback) {
+        setTimeout(function() {
+            $log.log("Thingy")
+            if(sim.ws.readyState === 1) {
+                $log.log("Connection to websocket established");
+                if(callback!=null && typeof callback==='function') {
+                    callback();
+                }
+                return
+            } else {
+                sim.wait_for_connection(callback);
+            }
+        }, 500);
+
+    }
+
+    sim.start_simulation = function(map_name) {
+        $log.info("Successful call in service");
+        sim.ws.send("S:" + map_name);
+    }
+
+    sim.is_connected = function() {
+        return sim._ws_open;
+    }
+
     return sim;
-});
+}]);
